92cf1255b1fea6dd6ddc14beb791af0deae3c963
armaanbindra <emailme@armaanbindra.com>
2015-09-08 09:01:26 -0500

    Added a Demo File

---
+++ Demo.js
@@ -0,0 +1,19 @@
+myVoiceIt = require('VoiceIt');
+
+myVoiceIt.initialize('123456');
+//Pass your 6 digit developer id as parameter to the intialize method like shown above
+myVoiceIt.createUser({
+	email: 'developer@voiceit-tech.com',
+	password: 'd0CHipUXOk',
+	firstName: 'John',
+	lastName: 'Doe',
+	phone1:'530-946-4351',
+	phone2:'530-654-3464',
+	//The API Call also accepts a phone3 argument if desired
+	callback: function(response){
+	//ADD CUSTOM CODE HERE TO USE
+	//DATA RECEIVED IN THE response VARIABLE
+	console.log("The Server Responded with the JSON: ",response);
+  //The Server Responded with the JSON: { "Result" : "Success" }
+	}
+});
