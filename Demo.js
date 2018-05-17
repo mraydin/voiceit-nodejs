a1b5a24f12d5ce80a037681565da86bda7fcbc8b
Armaan Bindra <emailme@armaanbindra.com>
2018-05-17 15:18:21 -0500

    Fixed Get Enrollments Count Method to Accept Content Language

--- Demo.js
+++ Demo.js
@@ -1,19 +1,24 @@
 myVoiceIt = require('VoiceIt');
-
 myVoiceIt.initialize('DEVELOPER_ID_HERE');
-//Pass your 6 digit developer id as parameter to the intialize method like shown above
-myVoiceIt.createUser({
-	email: 'developer@voiceit-tech.com',
-	password: 'd0CHipUXOk',
-	firstName: 'John',
-	lastName: 'Doe',
-	phone1:'530-946-4351',
-	phone2:'530-654-3464',
-	//The API Call also accepts a phone3 argument if desired
-	callback: function(response){
-	//ADD CUSTOM CODE HERE TO USE
-	//DATA RECEIVED IN THE response VARIABLE
-	console.log("The Server Responded with the JSON: ",response);
-  //The Server Responded with the JSON: { "Result" : "Success" }
-	}
-});
+//Pass your developerId as parameter to the intialize method like shown above
+// myVoiceIt.createUser({
+// 	userId: 'developerUserId',
+// 	password: 'd0CHipUXOk',
+// 	//The API Call also accepts a phone3 argument if desired
+// 	callback: function(response){
+// 	//ADD CUSTOM CODE HERE TO USE
+// 	//DATA RECEIVED IN THE response VARIABLE
+// 	console.log("The Server Responded with the JSON: ",response);
+//   //The Server Responded with the JSON: { "Result" : "Success" }
+// 	}
+// });
+
+myVoiceIt.getEnrollmentsCount({
+		userId: 'developerUserId',
+		password: 'd0CHipUXOk',
+		contentLanguage: 'en-US',
+		phrase:'never forget tomorrow is a new day',
+		callback: function(response){
+			console.log(response);
+		}
+})
